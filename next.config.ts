f3bddd3fd625111440fe4aaa4dfbf202c2807e3c
calc <maheshwarivinayak90@gmail.com>
2025-08-03 16:15:45 +0530

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
